2b4466f867ce96344cfeae661095ab95f295d4e4
cameronyoung94 <68090343+cameronyoung94@users.noreply.github.com>
2021-03-10 21:58:42 -0500

    made the delete for the server

--- api/server.js
+++ api/server.js
@@ -36,4 +36,19 @@ server.get('api/users', async (req, res) => {
  }
 })
 
+server.delete('api/users/:id', async (req, res) => {
+
+ const { id } = req.params
+
+ try{
+  const user = await User.delete(id)
+  if (user) {
+   res.status(200).json(user)
+  } else {
+   res.status(404).json({ message: 'unknown id' })
+  }
+ } catch (err) {
+  res.status(500).json({ error: err.message })
+ }
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
