76a3bd9aa65d11d5109e59cda4c1119b84337300
cameronyoung94 <68090343+cameronyoung94@users.noreply.github.com>
2021-03-10 21:50:37 -0500

    added the dependencies and created the get and put for the server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,39 @@
 // BUILD YOUR SERVER HERE
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+
+const server= express();
+
+server.use(express.json());
+
+
+server.post('api/users', async (req, res) => {
+ const user = req.body;
+
+ if (!user.name || !user.bio) {
+  res.status(400).json({ message: 'must include name and bio' })
+
+ }else {
+  try {
+   const newCreatedUser = await User.create(user)
+
+   res.status(200).json(newCreatedUser)
+  } catch (err) {
+
+   res.status(500).json({ error: err.message })
+  }
+ }
+})
+
+server.get('api/users', async (req, res) => {
+ 
+ try{
+  const users =  await User.findAll()
+  res.status(200).json(users)
+ } catch (err) {
+  res.status(500).json({ error: err.message})
+ }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
